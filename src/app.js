996386f91929c3846543221efea1d0b071edbeed
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-18 19:42:31 +0000

    logic show the randoms

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
+  colorSuit();
 };
+
+const suits = ["♦", "♥", "♠", "♣"];
+const cards = ["A", 2, 3, 4, 5, 6, 7, 8];
+
+// function random
+function randomCard() {
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  let card = cards[Math.floor(Math.random() * cards.length)];
+
+  document.getElementById("randomCard").innerHTML = card;
+  document.getElementById("suitCard").innerHTML = suit;
+  function colorSuit() {
+    if (suits === "♥" || "♦") {
+      document.querySelector("#suitCard").style.color = "red";
+    } else if (suits === "♠" || "♣") {
+      document.querySelector("#suitCard").style.color = "black";
+    }
+  }
+}
+
+const suits = [":hearts:", ":spades:", ":clubs:", ":diamonds:"];
+const cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+window.onload = function() {
+  //write your code here
+  randomCard();
+};
+
+//function to change color on red or black
+
+//pseudocode
+// crear html basico
+// crear dos arrays
+// crear funcion random variables
+// crear el boton para imprimir
