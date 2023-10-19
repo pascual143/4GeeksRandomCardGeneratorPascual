ed09beffd9b081fb9b771b8366a88911f342efc4
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-19 10:38:25 +0000

    convert string to array adding randomSuits

--- src/app.js
+++ src/app.js
@@ -2,31 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
+const suits = ["♥", "♦", "♠", "♣"];
+const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
 window.onload = function() {
   //write your code here
   randomCard();
-  colorSuit();
-};
+  //colorSuit();
 
-const suits = ["♦", "♥", "♠", "♣"];
-const cards = ["A", 2, 3, 4, 5, 6, 7, 8];
+  // function random
+  function randomCard() {
+    let suit = Math.floor(Math.random() * suits.length);
+    let card = Math.floor(Math.random() * cards.length);
 
-// function random
-function randomCard() {
-  let suit = suits[Math.floor(Math.random() * suits.length)];
-  let card = cards[Math.floor(Math.random() * cards.length)];
+    let randomSuit = suits[suit];
+    let randomCard = cards[card];
 
-  document.getElementById("randomCard").innerHTML = card;
-  document.getElementById("suitCard").innerHTML = suit;
-  function colorSuit() {
-    if (suits === "♥" || "♦") {
+    document.getElementById("randomCard").innerHTML = randomCard;
+    document.getElementById("suitCard").innerHTML = randomSuit;
+
+    //var colorSuits = colorSuit();
+    if (randomSuit === "♥" || randomSuit === "♦") {
       document.querySelector("#suitCard").style.color = "red";
-    } else if (suits === "♠" || "♣") {
+    } else if (randomSuit === "♠" || randomSuit === "♣") {
       document.querySelector("#suitCard").style.color = "black";
     }
   }
-}
-
+};
 //function to change color on red or black
 
 //pseudocode
